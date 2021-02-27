508c9e9046185fa1dd80cc30944ea4935e271e76
rami-pro <77512307+rami-pro@users.noreply.github.com>
2021-02-27 22:54:51 +0100

    adding History section

--- src/components/ui/About.js
+++ src/components/ui/About.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import { Grid, Typography, useMediaQuery } from "@material-ui/core";
+import { Grid, Typography, Avatar, useMediaQuery } from "@material-ui/core";
+
+import history from "../../assets/history.svg";
 
 const useStyles = makeStyles((theme) => ({
   rowContainer: {
     paddingLeft: "5em",
     paddingRight: "5em",
-    marginBottom: "10em",
     [theme.breakpoints.down("sm")]: {
       paddingLeft: "1.5em",
       paddingRight: "1.5em",
@@ -16,6 +17,8 @@ const useStyles = makeStyles((theme) => ({
     fontStyle: "italic",
     fontWeight: 300,
     maxWidth: "50em",
+    fontSize: "1.5em",
+    lineHeight: 1.4,
   },
 }));
 
@@ -23,14 +26,83 @@ function About() {
   const classes = useStyles();
   return (
     <Grid container direction="column">
-      <Grid item>
+      <Grid item className={classes.rowContainer} style={{ marginTop: "2em" }}>
         <Typography variant="h2" gutterBottom>
           About Us
         </Typography>
       </Grid>
-      <Grid item container direction="row" justify="center">
+      <Grid
+        item
+        container
+        direction="row"
+        justify="center"
+        className={classes.rowContainer}
+        style={{ marginBottom: "10em" }}
+      >
         <Grid item className={classes.missionContainer}>
-          <Typography variant="h4">About Us</Typography>
+          <Typography variant="h4">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+            maxime consequuntur ducimus corrupti! it amet consectetur
+            adipisicing elit. Adipisci maxime consequuntur asperiores optio
+            porro minima repudiandae ducimus corrupti!
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        style={{ marginBottom: "10em" }}
+      >
+        <Grid item container md direction="column" style={{ maxWidth: "35em" }}>
+          <Grid item>
+            <Typography variant="h2">History</Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              paragraph
+              style={{ fontWeight: 700, fontStyle: "italic" }}
+            >
+              We are the new kid in the block
+            </Typography>
+            <Typography variant="body1" paragraph>
+              adipisicing elit. Adipisci maxime consequuntur asperiores optio
+              porro minima repudiandae ducimus corrupti!
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+              maxime consequuntur ducimus corrupti! it amet consectetur
+              adipisicing elit. Adipisci maxime consequuntur asperiores optio
+              porro minima.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              adipisicing elit. Adipisci maxime consequuntur asperiores optio
+              porro minima repudiandae ducimus.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container justify="center" alignItems="center" md>
+          <img
+            src={history}
+            alt="qui pen setting on top of a book"
+            style={{ maxHeight: "22em" }}
+          />
+        </Grid>
+      </Grid>
+      <Grid item container direction="column">
+        <Grid item>
+          <Typography variant="h4" align="center">
+            History
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1" align="center">
+            Bob martin, Founder
+          </Typography>
+          <Typography variant="body1" align="center">
+            I started coding when I was 9 years old
+          </Typography>
         </Grid>
       </Grid>
     </Grid>
