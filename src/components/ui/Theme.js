275b6d23dd56fcec03f771e14a9b61a2c1fef5fb
rami-pro <77512307+rami-pro@users.noreply.github.com>
2021-03-02 23:41:12 +0100

    Update Theme.js

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -76,5 +76,27 @@ export default createMuiTheme({
       borderRadius: 50,
       borderColor: arcBlue,
     },
+    overrides: {
+      MuiInputLabel: {
+        root: {
+          color: arcBlue,
+          fontSize: "1rem",
+        },
+      },
+      MuiInput: {
+        root: {
+          color: arcGrey,
+          fontWeight: 300,
+        },
+        underline: {
+          "&:before": {
+            borderBottom: `2px solid ${arcBlue}`,
+          },
+          "&:hover:not($disabled):not($focused):not($error):before": {
+            borderBottom: `2px solid ${arcBlue}`,
+          },
+        },
+      },
+    },
   },
 });
